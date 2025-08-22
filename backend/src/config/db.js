74a89456e6541516460e1a64a7237aa8f86f3fea
San1000-Ark <hacksan1000@gmail.com>
2025-08-22 14:55:52 -0500

    database configuration

---
+++ backend/src/config/db.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//create connections with pool VAR
+const pool=mysql.createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit:10,
+    queueLimit:0
+})
+
+//verification initial connection
+pool.getConnection((err,conn)=>{
+    if(err){
+        console.error("Error to connect Mysql: ", err.message);
+    }else{
+        console.log("Connnected to Mysql ");
+        conn.release();
+    }
+})
+
+export default pool;
