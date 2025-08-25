7821ea4b5085220e7448ae47797c9032ea062265
San1000-Ark <hacksan1000@gmail.com>
2025-08-25 16:33:46 -0500

    correction backend-server

--- backend/server.js
+++ backend/server.js
@@ -23,31 +23,3 @@ const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`server initilized on http://localhost:${PORT}`);  
 })
-
-
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import pool from './src/config/db.js';
-
-//load of environment vars
-dotenv.config();
-const app=express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/',(req,res)=>{
-    res.send("API WORKING... ");
-})
-
-//routes
-
-
-//start server
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`server initilized on http://localhost:${PORT}`);  
-})
-
-
