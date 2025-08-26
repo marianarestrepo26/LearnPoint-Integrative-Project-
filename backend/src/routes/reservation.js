d3fb46f73be13b4b336096c8d3a68912ccbfa210
San1000-Ark <hacksan1000@gmail.com>
2025-08-26 15:56:03 -0500

    ADD

--- backend/src/routes/reservation.js
+++ backend/src/routes/reservation.js
@@ -1,4 +1,7 @@
-import mysql from 'mysql2'
+import express from 'express';
+import pool from '../config/db.js';
+
+const router=express.Router();
 
 router.post("/reservation", async (req, res) => {
   try {
@@ -6,10 +9,8 @@ router.post("/reservation", async (req, res) => {
 
     const [result] = await pool.query(
       `INSERT INTO reservation (reservation_date, tutor_availability_id, tutors_id, students_id, subjects_id) 
-       VALUES (?, ?, ?, ?, ?)`,
-      [reservation_date, tutor_availability_id, tutors_id, students_id, subjects_id]
+      VALUES (?, ?, ?, ?, ?)`, [reservation_date, tutor_availability_id, tutors_id, students_id, subjects_id]
     );
-
     res.json({ message: "Resevation created", id: result.insertId });
   } catch (err) {
     res.status(500).json({ error: err.message });
