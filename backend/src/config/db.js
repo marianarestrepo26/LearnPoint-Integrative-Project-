0b183869d9f2752b33cb6c90bb91b5e314b28fc8
San1000-Ark <hacksan1000@gmail.com>
2025-08-28 10:59:05 -0400

    TRY connections

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,26 +1,7 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import express from 'express';
+import pool from '../config/db.js'; // use the actually connnection
+import { Router } from 'express';
+import dotenv from "dotenv";
+
 dotenv.config();
 
-//create connections with pool VAR
-const pool=mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit:10,
-    queueLimit:0
-})
-
-//verification initial connection
-pool.getConnection((err,conn)=>{
-    if(err){
-        console.error("Error to connect Mysql: ", err.message);
-    }else{
-        console.log("Connnected to Mysql ");
-        conn.release();
-    }
-})
-
-export default pool;
