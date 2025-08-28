0869a9fedd27438ba5e550a8869fdebf4e93e7f1
San1000-Ark <hacksan1000@gmail.com>
2025-08-28 11:07:34 -0400

    add connection database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,3 +5,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
+// Create pool connections
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+export default pool;
+
+
+
